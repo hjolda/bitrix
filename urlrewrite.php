3529aefb7109a76773b2e8332a418a7181b69dfe
Marina Kuznetsova <hjolda@gmail.com>
2024-11-22 15:38:13 +0300

    bitr

--- urlrewrite.php
+++ urlrewrite.php
@@ -2,26 +2,10 @@
 $arUrlRewrite=array (
   0 => 
   array (
-    'CONDITION' => '#^/services/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/services/index.php',
-    'SORT' => 100,
-  ),
-  1 => 
-  array (
-    'CONDITION' => '#^/products/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/products/index.php',
-    'SORT' => 100,
-  ),
-  2 => 
-  array (
-    'CONDITION' => '#^/news/#',
+    'CONDITION' => '#^novosti/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
-    'PATH' => '/news/index.php',
+    'PATH' => '/index.php',
     'SORT' => 100,
   ),
 );
